e04ddbf814186c1b9f28aecc82b9655b290aa872
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2015-11-17 21:23:19 +0100

    Created styles and scripts with gulp.

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,36 +4,9 @@
     <title>Laravel</title>
 
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/all.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
 
-    <style>
-        html, body {
-            height: 100%;
-        }
-
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
-
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 96px;
-        }
-    </style>
 </head>
 <body>
 <div class="container">
@@ -76,4 +49,6 @@
     </div>
 </div>
 </body>
+<script src="{{ asset('js/main.js') }}"></script>
+<script src="{{ asset('js/all.js') }}"></script>
 </html>
